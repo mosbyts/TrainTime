c80f3e186bf428e26b690a084c3218178ea03f1e
Tianna Mosby <mosbyts@vcu.edu>
2019-09-21 14:05:36 -0400

    Momentjs is somewhat working. Still needs work.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,5 +1,5 @@
   // Web app's Firebase configuration
-  var firebaseConfig = {
+  /*var firebaseConfig = {
     apiKey: "AIzaSyCAScvXSzoSNgX3spM5WkrWwfHwfeHjYgE",
     authDomain: "week-7-firebase-practice.firebaseapp.com",
     databaseURL: "https://week-7-firebase-practice.firebaseio.com",
@@ -11,20 +11,72 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  var database = firebase.database();*/
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCtRAqppqCcGyEFMrlx3CzwDPCcYhSTSeY",
+    authDomain: "traintime-43604.firebaseapp.com",
+    databaseURL: "https://traintime-43604.firebaseio.com",
+    projectId: "traintime-43604",
+    storageBucket: "traintime-43604.appspot.com",
+    messagingSenderId: "51100514373",
+    appId: "1:51100514373:web:38f2981aeb8b5e153f8b2c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
   $("#addTrain").on("click", function(event){
       event.preventDefault();
-  });
+      
+      var trainName = $("#trainName").val();
+      var trainDestination = $("#trainDestination").val();
+      var trainTime = $("#trainTime").val();
+      var trainFrequency = $("#trainFrequency").val();
+      
+      var trainInfo = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+      };
+      
+      database.ref().push(trainInfo);
+      
+      console.log(trainInfo.name);
+      console.log(trainInfo.destination);
+      console.log(trainInfo.time);
+      console.log(trainInfo.frequency);
 
-  var trainName = $("#trainName").val().trim();
-  var trainDestination = $("#trainDestination").val().trim();
-  var trainTime = $("#trainTime").val().trim();
-  var trainFrequency = $("#trainFrequency").val().trim();
+      alert("Train successfully added.");
 
-  var trainInfo = {
-    name: trainName,
-    destination: trainDestination,
-    time: trainTime,
-    frequency: trainFrequency
-  };
+      var trainName = $("#trainName").val("");
+      var trainDestination = $("#trainDestination").val("");
+      var trainTime = $("#trainTime").val("");
+      var trainFrequency = $("#trainFrequency").val("");
+});
 
-  database.ref().push(trainInfo);
+database.ref().on("child_added", function(childSnapshot){
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val(). destination;
+  var trainFrequency = childSnapshot.val().frequency;
+  
+  var format = "h:mm";
+  
+  var trainTime = childSnapshot.val().time;
+  var trainTimeConversion = moment(trainTime, format);
+  var trainTimeHTML = trainTimeConversion.toNow();
+
+  var trainArrival = moment(trainFrequency, format);
+  
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDestination),
+    $("<td>").text(trainFrequency),
+    $("<td>").text(trainTimeHTML),
+    $("<td>").text(trainArrival)
+  );
+
+  $(".table").append(newRow);
+})
